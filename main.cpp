be5e323d832daefcd9bd1f7ad9dc5374e576cd42
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-06-06 18:36:09 +0530

    "Lecture 3 first consition program"

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 15;
+
+    if (a > 0)
+    {
+        cout << "Positive Number";
+    }
+    else
+    {
+        cout << "Negative Number";
+    }
+
+    return 0;
+}
