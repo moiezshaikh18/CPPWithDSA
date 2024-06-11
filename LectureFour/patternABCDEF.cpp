fecf8a95bb0bbd77ae5920865b86ba75d21e4a9d
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-06-11 22:08:12 +0530

    some patterns

---
+++ LectureFour/patternABCDEF.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number:";
+    cin >> n;
+
+    int i = 1;
+
+    char ch = 'A';
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << ch;
+            ch = ch + 1;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
