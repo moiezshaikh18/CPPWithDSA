5a6370d289e73f876b457010addce54651aea599
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-08-11 17:05:05 +0530

    Binary to Decimal converstion

---
+++ LectureSixBinaryDecimalNumberSystem/BinarytoDecimal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n, ans = 0;
+
+    cout << "Entet Binary Number:- ";
+    cin >> n;
+
+    int i = 0;
+    while (n != 0)
+    {
+
+        int digit = n % 10;
+
+        if (digit == 1)
+        {
+            ans = ans + pow(2, i);
+        }
+        n = n /10;
+        i++;
+    }
+    cout << ans;
+}
