6bec5bb4be8743565b584f0e53123e0e5df9f00d
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-11-22 22:52:22 +0530

    Prime Number with function

---
+++ RestartNovCpp/SwitchAndFunctions/Function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+bool isPrime(int num){
+    if(n % 2 != 0){
+        cout<<"Prime Number!!";
+    }else{
+        cout<<"Not Prime";
+    }
+}
+
+int main(){
+    
+}
