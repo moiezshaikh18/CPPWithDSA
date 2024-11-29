0a531cd54bf6f583315861b011888056bb027eeb
mueej <v-mueej@fieldera.com>
2024-11-29 17:57:17 +0530

    snippet

---
+++ RestartNovCpp/Arrays/revision.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter the size of array::" << endl;
+    cin >> size;
+
+    int array[100];
+
+    cout << "Please enter the " << size << " elements::" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    return 0;
+}
