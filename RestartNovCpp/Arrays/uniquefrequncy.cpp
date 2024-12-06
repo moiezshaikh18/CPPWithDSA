6c950cd79123756fcd4d041d603998d467c0625e
Moiz shaikh <81212506+moiezshaikh18@users.noreply.github.com>
2024-12-06 17:36:02 +0530

    Create uniquefrequncy.cpp

---
+++ RestartNovCpp/Arrays/uniquefrequncy.cpp
@@ -0,0 +1,61 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+bool areFrequenciesUnique(int array[],int size ){
+    
+     int count[201]={0};
+    int freq[201]={0};
+    
+    for(int i = 0;i<size;i++){
+        int mappedIndex = array[i] + 100;
+        count[mappedIndex] = count[mappedIndex] + 1; // 0 = 0 + 1; 1 = 1 + 1 = 2,;
+    }
+    
+    
+    for(int i = 0; i <201;i++){
+        if(count[i] > 0){ // count[101] ==> 1 ,2;
+            if(freq[count[i]] > 0){ // freq[1] ==> value is 0
+                return false;
+            }
+            
+            freq[count[i]] = freq[count[i]] +1; // 0  = 0 +1;
+        }
+    }
+    
+    return true;
+}
+
+
+
+int main() {
+    int size;
+    cout << "Enter the size of array" << endl;
+    cin>>size;
+    
+    int array[100]; // Array with a maximum size of 100
+   
+    
+    cout << "Enter the " << size << " elements:" << endl;
+    
+    for(int i = 0; i < size; i++) {
+        cin>> array[i]; // Input elements of the array
+    }
+    
+    
+ if (areFrequenciesUnique(array, size)) {
+        cout << "All frequencies are unique!" << endl;
+    } else {
+        cout << "Frequencies are not unique!" << endl;
+    }    
+    
+    
+    cout << "Array elements are: ";
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << " "; // Print each element of the array
+    }
+    cout << endl; // End line after printing all elements
+
+    return 0;
+}
