d23fc2134bc90cf390041b44df28607718ffb38a
Moiz shaikh <81212506+moiezshaikh18@users.noreply.github.com>
2025-01-06 22:04:33 +0530

    Some of array

--- RestartNovCpp/Arrays/sample3.cpp
+++ RestartNovCpp/Arrays/sample3.cpp
@@ -1,54 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void sort01(int arr[], int size)
-{
-
-    int left = 0, right = size - 1;
-
-    while (left < right)
-    {
-        while (arr[left] == 0 && left < right)
-        {
-            left++;
-        }
-        while (arr[right] == 1 && left < right)
-        {
-            right--;
-        }
-        if (arr[left] == 1 && arr[right] == 0)
-        {
-            int temp = 0;
-            temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
-    }
-}
-
 int main()
 {
     int size;
-    cout << "Enter the size of array::" << endl;
+
+    cout << "Enter the size of array: ";
     cin >> size;
 
-    int array[100];
-    // for input array
+    if (size <= 0 || size > 100)
+    {
+        cout << "Invalid size. Please enter a size between 1 and 100." << endl;
+        return 1; // Exit the program with an error
+    }
 
-    cout << "Enter the " << size << " elements in array::" << endl;
+    int array[100];
+
+    cout << "Enter " << size << " elements:" << endl;
     for (int i = 0; i < size; i++)
     {
         cin >> array[i];
     }
 
-    sort01(array, size);
+    int sum = 0;
 
     for (int i = 0; i < size; i++)
     {
-        cout << array[i] << " ";
+        sum = sum + array[i];
     }
 
-    return 0;
+    cout << "Sum of array is : " << sum << endl;
 }
